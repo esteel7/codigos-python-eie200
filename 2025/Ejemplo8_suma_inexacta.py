f52d6b93e12e0b0561af74d76a51b456c1c962d9
esteel7 <rodrigodr7@gmail.com>
2025-09-17 21:27:27 -0300

    Se agregan ejemplos de diferencias finitas y cálculos inexactos

---
+++ 2025/Ejemplo8_suma_inexacta.py
@@ -0,0 +1,6 @@
+numero = 0.0001
+N_max = 10000
+suma = 0
+for i in range(0,N_max):
+    suma += numero
+print(f"El resultado de sumar {numero}, {N_max} veces es:\n {suma}")
---
+++ testing/calculos_largos.py
@@ -0,0 +1,7 @@
+
+n_max = 10000
+suma = 0
+for i in range(1,n_max):
+    suma+=0.0001
+
+print(f"El resultado de sumar 0.0001 {n_max} veces es {suma}")
