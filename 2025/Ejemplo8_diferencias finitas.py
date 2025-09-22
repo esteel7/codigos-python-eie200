97c71c9a259b1a727f9f9e6f16ff7c6ac6979ee5
esteel7 <rodrigodr7@gmail.com>
2025-09-22 12:24:21 -0300

    Se actualiza el Ejemplo8_diferencias finitas

--- 2025/Ejemplo8_diferencias finitas.py
+++ 2025/Ejemplo8_diferencias finitas.py
@@ -1,10 +1,8 @@
-import numpy as np
-
 # Definir la función f(x) y su derivada exacta f'(x)
 def f(x):
     return -0.1*x**4 - 0.15*x**3 - 0.5*x**2 - 0.25*x + 1.2
 
-def f_prime_exact(x):
+def f_derivada(x):
     return -0.4*x**3 - 0.45*x**2 - 1.0*x - 0.25
 
 # Definir las funciones para calcular la derivada usando diferencias finitas
@@ -19,6 +17,8 @@ def centered_difference(f, x, h):
 
 # Valor en el que se evalúa la derivada
 x = 0.5
+# Cálculo de la derivada exacta en x = 0.5
+print(f"\nDerivada exacta en x = {x}: {f_derivada(x):.6f}")
 
 # Valores de h a usar
 h_values = [0.5, 0.25, 0.125, 0.0625, 0.03125, 0.015625]
@@ -34,7 +34,3 @@ for h in h_values:
     print(f"Diferencia finita hacia adelante: {fwd_diff:.6f}")
     print(f"Diferencia finita hacia atrás: {bwd_diff:.6f}")
     print(f"Diferencia finita centrada: {ctr_diff:.6f}")
-
-# Cálculo de la derivada exacta en x = 0.5
-f_prime_exact_value = f_prime_exact(x)
-print(f"\nDerivada exacta en x = {x}: {f_prime_exact_value:.6f}")
