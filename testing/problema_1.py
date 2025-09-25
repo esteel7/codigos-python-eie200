5b89f62bc728df01409eeebd045caf7d32b31f96
esteel7 <rodrigodr7@gmail.com>
2025-09-25 10:01:23 -0300

    Se modifica el problema_1.py

--- testing/problema_1.py
+++ testing/problema_1.py
@@ -19,6 +19,7 @@ x = np.linalg.solve(A, b)
 
 i1, i2 = x
 
+print(f"Supuesto D1: ON y D2: ON (CORRECTO)")
 print(f"i1 = {i1:.6f} A e i2 = {i2:.6f} A")         # i1 = -0.027744 A e i2 = -0.003922 A
 print(f"Id1 = {i2-i1:.6f} A e Id2 = {-i2:.6f} A")   # Id1 = 0.023822 A e Id2 = 0.003922 A
 print(f"Vo = {R3*i2-V2:.4f} V\n")                   # Vo = -11.1963 V
@@ -38,6 +39,7 @@ x = np.linalg.solve(A, b)
 
 i1, i2 = x
 
+print(f"Supuesto D1: RUPTURA y D2: ON")
 print(f"i1 = {i1:.6f} A e i2 = {i2:.6f} A")           # i1 = -0.033003 A e i2 = -0.001856 A
 print(f"Id1 = {i2-i1:.6f} A e Id2 = {-i2:.6f} A\n")   # Id1 = 0.031147 A e Id2 = 0.001856 A
 
@@ -49,6 +51,7 @@ i1 = - (V1 + Vz) / (R1 + R2)
 
 i2 = 0
 
+print(f"Supuesto D1: RUPTURA y D2: OFF")
 print(f"i1 = {i1:.6f} A e i2 = 0 A")    # i1 = -0.031739 A e i2 = 0 A
 print(f"Id1 = {-i1:.6f} A\n")             # Id1 = 0.031739 A
 
@@ -58,5 +61,6 @@ print(f"Id1 = {-i1:.6f} A\n")             # Id1 = 0.031739 A
 
 i = (Vj + V2 - V1) / (R1 + R3)
 
+print(f"Supuesto D1: OFF y D2: ON")
 print(f"i = {i:.6f} A")                             # i = -0.010641 A
 print(f"Vd1 = {V1 + R1*i:.6f} e Id2 = {-i:.6f} A")  # Vd1 = 15.658974 e Id2 = 0.010641 A
