de411c70ed5d5284dbdaa7b0c20af2d34bb5c0e7
esteel7 <rodrigodr7@gmail.com>
2025-10-06 10:45:21 -0300

    Se actualiza y completa Ejemplo9.py

--- 2025/Ejemplo9.py
+++ 2025/Ejemplo9.py
@@ -1,39 +1,39 @@
+#Importar las librerías que usaremos (numpy y matplotlib)
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Definimos la función que representa la derivada dy/dt = f(t, y)
-def dydt(t, y):
-    return 4 * np.exp(0.8 * t) - 0.5 * y
+# Definir la función que representa la expresión de la derivada dy/dt = f(t,y)
+def dydt(t,y):
+    return 4*np.exp(0.8*t)-0.5*y 
 
-# Definimos la solución exacta
+# Podemos definir la función exacta, con el objetivo de comparar nuestros resultados
 def y_exacta(t):
-    return (4 / 1.3) * (np.exp(0.8 * t) - np.exp(-0.5 * t)) + 2 * np.exp(-0.5 * t)
+    return (4/1.3)*(np.exp(0.8*t)-np.exp(-0.5*t))+2*np.exp(-0.5*t)
 
-# Condiciones iniciales
-t0 = 0
-y0 = 2
-h = 0.5
+# Definimos parámetros y condiciones iniciales
+t_inicio = 0
 t_final = 4
+y0 = 2
+h = 1
+# Inicializamos los vectores de t e y. Ayuda: Usar numpy.arange() si h es un float
+t = np.arange(t_inicio,t_final+h,h) # t = [0, 1.0, 2.0, 3.0, 4.0]
+y = np.zeros_like(t)                # y = [0 0 0 0 0]
+y[0] = y0                           # y = [2 0 0 0 0]
 
-# Inicializamos los valores de t y y
-t = np.arange(t0, t_final + h, h)
-y = np.zeros_like(t)
-y[0] = y0
+# Implementamos el método de Euler, por ejemplo, con un for
+for i in range(len(t)-1): # itera con i = 0, 1, 2, 3
+    y[i+1] = y[i]+dydt(t[i],y[i])*h
 
-# Implementamos el método de Euler
-for i in range(len(t)-1):
-    y[i+1] = y[i] + dydt(t[i],y[i])* h
-
-# Calculamos la solución exacta
-t_exact = np.linspace(t0, t_final, 100)
+# Obtenemos la solución exacta
+t_exact = np.linspace(t_inicio, t_final, 100)
 y_ref = y_exacta(t_exact)
 
-# Graficamos los resultados
-plt.plot(t, y, 'bo-', label='Método de Euler')
+# Graficamos y comparamos los resultados obtenidos con la solución exacta
+plt.plot(t, y,'bo-', label='Método de Euler')
 plt.plot(t_exact, y_ref, 'r-', label='Solución Exacta')
 plt.xlabel('t')
 plt.ylabel('y(t)')
-plt.title('Comparación: Método de Euler vs. Solución Exacta')
+plt.title('Comparación: Método de Euler vs Solución Exacta')
 plt.legend()
 plt.grid(True)
 plt.show()
